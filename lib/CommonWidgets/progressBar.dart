3930c6e802ba6077c497a99b92e0e3c517bd3d7c
José Alberto Espinoza Morelos <spartan73rmy12@gmail.com>
2020-07-20 09:15:15 -0500

    add preview and printer PDF

---
+++ lib/CommonWidgets/progressBar.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+void loading(BuildContext context) {
+  showDialog(
+    context: context,
+    barrierDismissible: false,
+    builder: (BuildContext context) {
+      return Dialog(
+        child: new Row(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            new CircularProgressIndicator(),
+            new Text("Loading"),
+          ],
+        ),
+      );
+    },
+  );
+  new Future.delayed(new Duration(seconds: 3), () {
+    Navigator.pop(context); //pop dialog
+  });
+}
